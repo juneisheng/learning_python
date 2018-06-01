371542d78a5bc96837ef8e73919193029d995b6c
juneisheng <yinzhicheng@yahoo.com>
2018-06-01 22:20:57 +0900

    git

---
+++ map_reduce_2.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+def prod(L):
+    def times(x,y):
+        return x * y
+    
+    return reduce(times, L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
